76f1fbd74fe4eb3b015e3de113762d7581850a1a
0x0FACED <https.genius@gmail.com>
2024-07-09 12:15:13 +0300

    added: get DNS servers, add DNS server, remove DNS server methods

--- pkg/service/host_service.go
+++ pkg/service/host_service.go
@@ -2,16 +2,41 @@ package service
 
 import (
 	"context"
-	"grpc-gateway/pkg/util"
+	api "grpc-gateway/api/generated"
 )
 
 type HostService struct {
+	api.UnimplementedHostServiceServer
 }
 
 func (s *HostService) SetHostname(ctx context.Context, req *api.SetHostnameRequest) (*api.SetHostnameResponse, error) {
-	err := util.SetHostname(req.Hostname)
+	err := setHostname(req.Hostname)
 	if err != nil {
 		return &api.SetHostnameResponse{Success: false}, err
 	}
 	return &api.SetHostnameResponse{Success: true}, nil
+}
+
+func (s *HostService) GetDNSServers(ctx context.Context, req *api.GetDNSServersRequest) (*api.GetDNSServersResponse, error) {
+	srvs, err := getDNSServers()
+	if err != nil {
+		return nil, err
+	}
+	return &api.GetDNSServersResponse{Servers: srvs}, nil
+}
+
+func (s *HostService) AddDNSServer(ctx context.Context, req *api.AddDNSServerRequest) (*api.AddDNSServerResponse, error) {
+	err := addDNSServer(req.Server)
+	if err != nil {
+		return &api.AddDNSServerResponse{Success: false}, err
+	}
+	return &api.AddDNSServerResponse{Success: true}, err
+}
+
+func (s *HostService) RemoveDNSServer(ctx context.Context, req *api.RemoveDNSServerRequest) (*api.RemoveDNSServerResponse, error) {
+	err := removeDNSServer(req.Server)
+	if err != nil {
+		return &api.RemoveDNSServerResponse{Success: false}, err
+	}
+	return &api.RemoveDNSServerResponse{Success: true}, nil
 }
